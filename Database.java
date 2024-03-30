d13e9df3f2e8dc172ae2f3346eeafe0db4c5f032
heartbug13 <149316541+heartbug13@users.noreply.github.com>
2024-03-29 20:56:47 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -4,6 +4,6 @@ public interface Database {
     User getUserByUsername(String username);
     void addPost(Post post);
     List<Post> getPostsByUser(User user);
-    Void addCommendToPost(Comment comment, Post post);
+    void addCommendToPost(Comment comment, Post post);
     List<Comment> getCommentsForPost(Post post);
 }
