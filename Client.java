b21b810d90372dd49e9ee6b5d45e979bc3c1ee51
ehssan-ak13 <132846042+ehssan-ak13@users.noreply.github.com>
2024-04-15 00:34:51 -0400

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,36 @@
+/**
+ * Group Project - Social Media
+ *
+ * recreates a social media platform
+ *
+ * @author Samantha Grief, Connor Mccarthy, Ehssan Kanamkandy, Jaheim Cruickshank, L10
+ *
+ * @version April 1, 2024
+ *
+ */
+public class Client implements Clients {
+    private User makeUser;
+    private Profile makeProfile;
+    private Post makePost;
+    private Comment makeComment;
+
+    @Override
+    public User makeUser() {
+        return null;
+    }
+
+    @Override
+    public Profile makeProfile() {
+        return null;
+    }
+
+    @Override
+    public Post makePost() {
+        return null;
+    }
+
+    @Override
+    public Comment makeComment() {
+        return null;
+    }
+}
