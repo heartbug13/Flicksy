4bfe2ce79062f4dafc5f3c6de7fdf5ecf8d3a022
ehssan-ak13 <132846042+ehssan-ak13@users.noreply.github.com>
2024-04-14 23:53:49 -0400

    ClientHandler.java

---
+++ ClientHandler.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler implements Runnable{
+
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //**
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true); //**
+
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null) {
+
+                System.out.println("Received from client: " + inputLine);
+                writer.println("Server received: " + inputLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally { //**
+            try {
+                clientSocket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
